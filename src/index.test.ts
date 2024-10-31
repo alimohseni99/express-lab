31ecfed8c636e9969a7492110376a3ab9f79cf01
Ali Mohseni <ali.mohseni@appliedtechnology.se>
2024-10-31 09:34:17 +0100

    breaked out app from testing first endpoint

--- src/index.test.ts
+++ src/index.test.ts
@@ -2,14 +2,12 @@ import { deepEqual } from "node:assert/strict";
 import test from "node:test";
 import request from "supertest";
 import express from "express";
+const app = express();
 
+app.get("/status", (req, res) => {
+  res.json({ status: "ready" });
+});
 test("GET /status", async () => {
-  const app = express();
-
-  app.get("/status", (req, res) => {
-    res.json({ status: "ready" });
-  });
-
   const result = await request(app).get("/status");
 
   deepEqual(result.status, 200);
